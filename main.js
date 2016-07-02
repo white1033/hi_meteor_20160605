e3da158ed60829f147a2bc661e9ada6b66d42371
white1033 <k75715@gmail.com>
2016-07-02 08:54:55 +0800

    use username instead of email

--- main.js
+++ main.js
@@ -1,9 +1,46 @@
+Messages = new Mongo.Collection('messages')
+
 if (Meteor.isClient) {
-    Template.body.helpers({
-        messages: [
-            {text: 'hihi kkd', num: 1},
-            {text: 'hihi yen han', num: 2},
-            {text: 'hihi aya', num: 3},
-        ]
-    })
+
+  Meteor.startup(function() {
+    Accounts.ui.config({
+      passwordSignupFields: 'USERNAME_ONLY',
+    });
+  });
+
+  Meteor.subscribe('messages', 5)
+
+  Template.body.helpers({
+    messages: function() {
+      return Messages.find()
+    }
+  })
+
+  Template.body.events({
+    'change #message': function(event) {
+      let text = $(event.target).val()
+      $(event.target).val('')
+      let message = { text }
+      Meteor.call('createMessage', message)
+    }
+  })
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    createMessage: function(message) {
+      let user = Meteor.userId()
+      if (user) {
+        message.username = Meteor.user().username
+        message.num = Messages.find().count() + 1
+        message.from = 'db'
+        message.createdAt = new Date()
+        Messages.insert(message)
+      }
+    }
+  })
+
+  Meteor.publish('messages', function(limit) {
+    return Messages.find({}, {sort: {createdAt: -1}, limit})
+  })
 }
